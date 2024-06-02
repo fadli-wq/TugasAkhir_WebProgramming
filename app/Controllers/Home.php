570fb9f41261342f89396e0ae25db6db135cf556
fadli-wq <dayfos123@gmail.com>
2024-06-02 22:22:32 +0700

    perubahan env dan perubahan controllers

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         return view('welcome_message');
     }
